f29f698df232789e9645e9fc212f19daef9c3c87
Kakyoin <1499384857@qq.com>
2025-03-20 16:11:41 +0800

    修复樱桃宝石转换燃料的BUG，并为樱桃宝石添加描述

--- src/main/java/com/Kakyoin17/cherrytinker/custom/CherryGemItem.java
+++ src/main/java/com/Kakyoin17/cherrytinker/custom/CherryGemItem.java
@@ -1,15 +1,23 @@
 package com.Kakyoin17.cherrytinker.custom;
 
 import com.Kakyoin17.cherrytinker.registry.ModFluids;
+import com.Kakyoin17.cherrytinker.until.RainbowText;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import slimeknights.tconstruct.fluids.TinkerFluids;
+
+import java.util.List;
 
 public class CherryGemItem extends Item  {
 
@@ -27,23 +35,35 @@ public class CherryGemItem extends Item  {
         long timeOfDay = level.getDayTime();
         if (context.getPlayer() instanceof Player){
             if (blockstate1 == TinkerFluids.blazingBlood.getBlock().defaultBlockState()){
-                if (timeOfDay>0&&timeOfDay<12000){
+                if ((timeOfDay/12000)/2==0){
                     itemStack.shrink(1);
                     level.setBlockAndUpdate(blockPos1,ModFluids.suncherryfuel.getBlock().defaultBlockState());
-                }return InteractionResult.sidedSuccess(level.isClientSide);
+                }return InteractionResult.CONSUME;
             }
             if (blockstate2 == TinkerFluids.magma.getBlock().defaultBlockState()){
-                if (timeOfDay>12000&&timeOfDay<24000){
+                if (timeOfDay%24000>12000){
                     itemStack.shrink(1);
                     level.setBlockAndUpdate(blockPos2,ModFluids.mooncherryfuel.getBlock().defaultBlockState());
-                }return InteractionResult.sidedSuccess(level.isClientSide);
+                }return InteractionResult.CONSUME;
             }
 
             }
-        return  InteractionResult.sidedSuccess(level.isClientSide);
+        return  InteractionResult.CONSUME;
     }
 
-    }
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag){
+        if(Screen.hasShiftDown()){
+            components.add(Component.translatable(
+                    ("At night, facing east, aim at the magma block to the west and right-click on the block")).withStyle(ChatFormatting.AQUA));}
+        else if(Screen.hasControlDown()){
+            components.add(Component.translatable(
+                    ("Facing east during the day, aim at the west block with Flame Blood, and right-click on the block")).withStyle(ChatFormatting.GOLD));
+
+    }else {
+            components.add(Component.translatable("Press SHIFT or CTRL for more info").withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(itemStack,level,components,tooltipFlag);
+    }}
 
 
 
